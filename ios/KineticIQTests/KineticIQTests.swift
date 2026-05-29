12080d086ec3cec8ef5b51924d5842da8d98a237
Jason Chillemi <jason.chillemi@gmail.com>
2026-05-28 17:16:33 -0700

    Add ios/ folder with XCTest setup (KineticIQTests)

---
+++ ios/KineticIQTests/KineticIQTests.swift
@@ -0,0 +1,31 @@
+//
+//  KineticIQTests.swift
+//  KineticIQTests
+//
+//  Created by Jason Chillemi on 5/28/26.
+//
+
+import XCTest
+
+final class KineticIQTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+      XCTAssertTrue(true)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
